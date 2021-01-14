5ca6fd469d8e92237327e4525e1617b01c979309
Yuri Alves <yurileader@gmail.com>
2021-01-14 12:49:29 -0300

    Configurando o projeto

---
+++ src/main/java/com/icetec/yurileader/vagasdevapi/VagasdevApiApplication.java
@@ -0,0 +1,13 @@
+package com.icetec.yurileader.vagasdevapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VagasdevApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VagasdevApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/icetec/yurileader/vagasdevapi/VagasdevApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icetec.yurileader.vagasdevapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VagasdevApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
