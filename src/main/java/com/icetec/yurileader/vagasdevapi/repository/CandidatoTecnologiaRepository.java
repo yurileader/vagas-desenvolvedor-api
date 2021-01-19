35e886ad13fe261865eb34069bb24ac804191139
Yuri Alves <yurileader@gmail.com>
2021-01-18 22:29:03 -0300

    Alterando FK de candidatoTecnologia

--- src/main/java/com/icetec/yurileader/vagasdevapi/repository/CandidatoTecnologiaRepository.java
+++ src/main/java/com/icetec/yurileader/vagasdevapi/repository/CandidatoTecnologiaRepository.java
@@ -5,6 +5,12 @@ import com.icetec.yurileader.vagasdevapi.model.CandidatoTecnologiaId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CandidatoTecnologiaRepository extends JpaRepository<CandidatoTecnologia, CandidatoTecnologiaId> {
+
+    List<CandidatoTecnologia> findByIdCandidato(Long idCandidato);
+
+    List<CandidatoTecnologia> findByIdTecnologia(Long idTecnologia);
 }
